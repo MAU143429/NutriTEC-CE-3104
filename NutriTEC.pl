eda20abe3c821e5c58acf55339f51af53ed5073c
MAU <mau141226@gmail.com>
2021-10-26 17:04:07 -0600

    [Update] NutriTEC files added

---
+++ NutriTEC.pl
@@ -0,0 +1,10 @@
+/** ---------- Tarea 2 NutriTEC CE-3104 ----------
+
+                    Gabriel Vargas
+                    Yendry Badilla
+                   Mauricio Calderon
+                   
+    ----------                          ----------
+*/
+
+
