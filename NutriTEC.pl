c0b5703cd8921161719d9779ad98dda9b09df7cd
MAU <mau141226@gmail.com>
2021-10-26 22:37:09 -0600

    [Added] Some diets added

--- NutriTEC.pl
+++ NutriTEC.pl
@@ -8,3 +8,83 @@
 */
 
 
+% Especificar datos de entrada.
+
+dieta('Dieta 6',"keto", "NA",'intermedio',5,'
+Puedes empezar con el siguiente plan alimenticio bajo en carbohidratos.\n
+Desayuno:   ½ aguacate picado en rodaja.\n
+            2 huevos + rebanada de tocino.\n
+            Preparar solo rociando aceite en spray (no oliva ni vegetal). \n
+Merienda Mañana:  ¼ pullado de semillas de girasol. \n
+        ½ naranja picada. \n
+Almuerzo: 2 tazas de ensalada Mixta.\n
+          2-3 hash Browns o tortitas de coliflor.\n
+          1 rebanada de pan keto.\n
+Merienda tarde: 1 taza de café o té. \n
+                30g de mantequilla de maní. \n
+                1 muffin keto de arándanos. \n
+Cena: ½ Tortilla de bacalao. \n
+      1 lata de atún mediana en agua. \n
+      1 aguacate en trozos. \n ').
+  
+dieta('Dieta 7',"Vegetariana", "Celiaquia",'intermedio',5,'
+Puedes empezar con el siguiente plan alimenticio vegetariano. \n
+Desayuno:   1 taza de leche de soya. \n
+            Pullado de pipas de girasol. \n
+            Banano en rebanadas. \n
+Merienda Mañana:  ½ taza de leche de coco. \n
+                  1 tostada de pan integral con mermelada. \n
+Almuerzo: 1 taza de Espaguetis con frutos secos. \n
+          ½ taza de sopa de quinua. \n
+          1 higo. \n
+Merienda tarde: 1 taza de jugo de naranja. \n
+                1 bocadillo de tofu y tomate. \n
+Cena: Ensalada de alga wakame con sésamo. \n
+      ½ rebanada de pan integral con pure y aguacate.\n ').
+  
+dieta('Dieta 8',"Vegana", "Dislipidemia",'intermedio',5,'
+Puedes empezar con el siguiente plan alimenticio vegano. \n
+Desayuno: 1 smoothie bowl de piña y coco con frutos rojos. \n
+          1 galleta de avena y manzana. \n
+          ½ pieza de frutas fresca. \n
+Merienda Mañana:  1 tazón de leche de soya . \n
+     		          4-5 Almendras sin azúcar y nueces. \n
+Almuerzo: 2 Albóndigas veganas de brócoli con garbanzos y arroz. \n
+          ½ taza de ensalada de vegetales. \n
+          1 trozo de mandarina. \n
+Merienda tarde: 1 vaso de bebida vegetal sin azúcar. \n
+                1 barrita de avena y frutos secos. \n
+Cena:  Salteado rápido de tofu y kale con sésamo. \n
+       ½ pimiento relleno de soja texturizada. \n ').
+  
+dieta('Dieta 9',"Proteica", "Diabetes",'intermedio',5,'
+Puedes empezar con el siguiente plan alimenticio alto en proteínas. \n
+Desayuno: 2 huevos revueltos. \n
+          2 galletas de trigo con mantequilla de maní. \n
+          1 taza de café. \n
+Merienda Mañana: ½ Banano . \n
+                 1 galleta de proteína Whey. \n
+Almuerzo: 1 taza de Ensalada mixta. \n
+          2 trozos de pechuga a la plancha. \n
+          1 higo. \n
+Merienda tarde: Batido de proteína y creatina. \n
+                3-4 Almendras sin azúcar. \n
+Cena: ½ taza de rodajas de tomate. \n
+      1 ½ de Filet de pescado al horno. \n
+      Papas salteadas con culantro. \n ').
+  
+dieta('Dieta 10',"NA", "Obesidad",'intermedio',5,'
+Puedes empezar con el siguiente plan alimenticio bajo en grasas. \n
+Desayuno: 2 huevos revueltos con aguacate y tomate. \n
+          Manzana en rebanadas. \n
+Merienda Mañana:  ½ taza de té verde. \n
+                  1 bolsita de palitos de zanahoria. \n
+Almuerzo: Estofado de ternera con verduras. \n
+          ½ taza pejibayes. \n
+Merienda tarde: 1 taza de café o té. \n
+                1 tostada con miel. \n
+Cena:  Verduras al horno. \n
+       Filet de res asado. \n
+       1 tortilla de maíz a la brasa. \n ').
+  
+  
