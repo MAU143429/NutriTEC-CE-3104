4fd85bd5da8aa654fbef91142371f54460c10365
MAU <mau141226@gmail.com>
2021-10-28 21:10:59 -0600

    [Update] Some files updated

--- NutriTEC.pl
+++ NutriTEC.pl
@@ -11,7 +11,7 @@
 %Parte 1 de la conversacion, reconocimiento de saludo o ausencia de etse
 %Comando para iniciar la conversacion.
 inicio():- nl, read_string(user_input, '\n', '.', _, Str),string_lower(Str,LowerCase),atomic_list_concat(Lista, ' ', LowerCase),incioConversacionAux(Lista,[]).
-incioConversacionAux(Lista,[]):-incioConversacion(Lista,[]),dietaSolicitada(Dieta),nivelEjercicio(Nivel),padecimientoSolicitado(Padecimiento),buscarDieta(Nombre,Dieta,Padecimiento,Nivel,Dias, Detalle).
+incioConversacionAux(Lista,[]):-incioConversacion(Lista,[]),dietaSolicitada(Dieta),nivelEjercicio(Nivel),padecimientoSolicitado(Padecimiento),buscarDieta(Nombre,Dieta,Padecimiento,Nivel, Detalle).
 incioConversacionAux(Lista,[]):-write("Saludo no detectado"),inicio().
 incioConversacion(Lista,Vacio):-saludo(Lista,Resto),nombre(Resto,Vacio).
 incioConversacion(Lista,Vacio):-saludo(Lista,Resto),horario(Resto,Vacio).
@@ -71,7 +71,7 @@ nivelEjercicio(Nivel):- write('Cuantos dias a la semana hace ejercicio?'),nl ,re
 
 
 %Base de Datos
-%Clasificacion del nivel del usuario segun la cantidad de Dieta que se hace a la semana
+%Clasificacion del nivel del usuario segun la cantidad de ejercicio que se hace a la semana
 nivel('principiante',["0","1","2"]).
 nivel('intermedio',["3","4"]).
 nivel('avanzado',["5","6","7"]).
@@ -82,99 +82,17 @@ buscarNivel(Nivel,Cantidad):- nivel(Nivel,Cantidades), miembro(Cantidad, Cantida
 miembro(X,[X|_]).
 miembro(X,[_|Y]):- miembro(X,Y).
 
+% Union
+
+buscarDieta(Nombre,Dieta,Padecimiento,Nivel, Detalle):- dieta(Nombre,Dieta,PadecimientoAux,Nivel, Detalle),Padecimiento\=PadecimientoAux,write('Le recomiendo la siguiente dieta: '),nl,write('Nombre de la dieta: '),write(Nombre),nl,write('Dias a la semana de entrenamiento: '),nl,write(Detalle).
+buscarDieta(Nombre,Dieta,Padecimiento,Nivel, Detalle):- write("Lo siento de momento no tengo ninguna dieta que se adapte a sus necesides"),nl.
+
 
 %dieta(nombreDieta,Dieta, padecimientos,nivel,dias,detalle).
 
-
-dieta('Dieta 6',"keto", "NA",'intermedio',1,'
-Puedes empezar con el siguiente plan alimenticio bajo en carbohidratos.\n
-Desayuno:   � aguacate picado en rodaja.\n
-            2 huevos + rebanada de tocino.\n
-            Preparar solo rociando aceite en spray (no oliva ni vegetal). \n
-Merienda Ma�ana:  � pullado de semillas de girasol. \n
-        � naranja picada. \n
-Almuerzo: 2 tazas de ensalada Mixta.\n
-          2-3 hash Browns o tortitas de coliflor.\n
-          1 rebanada de pan keto.\n
-Merienda tarde: 1 taza de caf� o t�. \n
-                30g de mantequilla de man�. \n
-                1 muffin keto de ar�ndanos. \n
-Cena: � Tortilla de bacalao. \n
-      1 lata de at�n mediana en agua. \n
-      1 aguacate en trozos. \n
-').
-
-dieta('Dieta 7',"vegetariana", "celiaquia",'intermedio',2,'
-Puedes empezar con el siguiente plan alimenticio vegetariano. \n
-Desayuno:   1 taza de leche de soya. \n
-            Pullado de pipas de girasol. \n
-            Banano en rebanadas. \n
-Merienda Ma�ana:  � taza de leche de coco. \n
-                  1 tostada de pan integral con mermelada. \n
-Almuerzo: 1 taza de Espaguetis con frutos secos. \n
-          � taza de sopa de quinua. \n
-          1 higo. \n
-Merienda tarde: 1 taza de jugo de naranja. \n
-                1 bocadillo de tofu y tomate. \n
-Cena: Ensalada de alga wakame con s�samo. \n
-      � rebanada de pan integral con pure y aguacate.\n
-').
-
-dieta('Dieta 8',"vegana", "dislipidemia",'intermedio',4,'
-Puedes empezar con el siguiente plan alimenticio vegano. \n
-Desayuno: 1 smoothie bowl de pi�a y coco con frutos rojos. \n
-          1 galleta de avena y manzana. \n
-          � pieza de frutas fresca. \n
-Merienda Ma�ana:  1 taz�n de leche de soya . \n
-                  4-5 Almendras sin az�car y nueces. \n
-Almuerzo: 2 Alb�ndigas veganas de br�coli con garbanzos y arroz. \n
-          � taza de ensalada de vegetales. \n
-          1 trozo de mandarina. \n
-Merienda tarde: 1 vaso de bebida vegetal sin az�car. \n
-                1 barrita de avena y frutos secos. \n
-Cena:  Salteado r�pido de tofu y kale con s�samo. \n
-       � pimiento relleno de soja texturizada. \n
-').
-
-dieta('Dieta 9',"proteica", "diabetes",'intermedio',4,'
-Puedes empezar con el siguiente plan alimenticio alto en prote�nas. \n
-Desayuno: 2 huevos revueltos. \n
-          2 galletas de trigo con mantequilla de man�. \n
-          1 taza de caf�. \n
-Merienda Ma�ana: � Banano . \n
-                 1 galleta de prote�na Whey. \n
-Almuerzo: 1 taza de Ensalada mixta. \n
-          2 trozos de pechuga a la plancha. \n
-          1 higo. \n
-Merienda tarde: Batido de prote�na y creatina. \n
-                3-4 Almendras sin az�car. \n
-Cena: � taza de rodajas de tomate. \n
-      1 � de Filet de pescado al horno. \n
-      Papas salteadas con culantro. \n
-').
-
-dieta('Dieta 10',"NA", "obesidad",'intermedio',5,'
-Puedes empezar con el siguiente plan alimenticio bajo en grasas. \n
-Desayuno: 2 huevos revueltos con aguacate y tomate. \n
-          Manzana en rebanadas. \n
-Merienda Ma�ana:  � taza de t� verde. \n
-                  1 bolsita de palitos de zanahoria. \n
-Almuerzo: Estofado de ternera con verduras. \n
-          � taza pejibayes. \n
-Merienda tarde: 1 taza de caf� o t�. \n
-                1 tostada con miel. \n
-Cena:  Verduras al horno. \n
-       Filet de res asado. \n
-       1 tortilla de ma�z a la brasa. \n
-').
-
-% Union
-
-buscarDieta(Nombre,Dieta,Padecimiento,Nivel,Dias, Detalle):- dieta(Nombre,Dieta,PadecimientoAux,Nivel,Dias, Detalle),Padecimiento\=PadecimientoAux,write('Le recomiendo la siguiente dieta: '),nl,write('Nombre de la dieta: '),write(Nombre),nl,write('Dias a la semana de entrenamiento: '),write(Dias),nl,write(Detalle).
-buscarDieta(Nombre,Dieta,Padecimiento,Nivel,Dias, Detalle):- write("Lo siento de momento no tengo ninguna dieta que se adapte a sus necesides"),nl.
-
-
-dieta('Dieta 1',"keto", "NA",'principiante',5,'
+               %----------------- Dieta 1 -----------------%
+               
+dieta('Dieta 1',"keto", "NA",'principiante','
 Puedes empezar con el siguiente plan alimenticio bajo en carbohidratos.\n
 Desayuno:   2 huevos revueltos con 30g de jam�n y 30g de queso. \n
 Merienda Media Ma�ana:  100g de zanahoria con aguacate. \n
@@ -185,7 +103,9 @@ Merienda tarde: Rodajas de aguacate con tomate y sal. \n
 Cena: 150g de salm�n al horno. \n
       100g de br�coli. \n').
 
-dieta('Dieta 2',"vegetariana", "NA",'principiante',5,'
+               %----------------- Dieta 2 -----------------%
+               
+dieta('Dieta 2',"vegetariana", "diabetes",'principiante','
 Puedes empezar con el siguiente plan alimenticio vegetariano.\n
 Desayuno:   1 vaso de leche de soja. \n
             Galletas de avena. \n
@@ -198,7 +118,9 @@ Merienda tarde: Batido de banano con leche de coco y semillas de girasol. \n
 Cena: Coliflor asada con espinacas, almendras y salsa de yogurt. \n
       Ensalada de vegetales frescos con semillas de ayote. \n').
 
-dieta('Dieta 3',"vegana", "NA",'principiante',5,'
+               %----------------- Dieta 3 -----------------%
+
+dieta('Dieta 3',"vegana", "dislipidemia",'principiante','
 Puedes empezar con el siguiente plan alimenticio vegano.\n
 Desayuno:   2 tostadas de pan integral. \n
             1 tomate rallado. \n
@@ -210,7 +132,9 @@ Almuerzo: Ensalada de hongos, espinaca y br�coli. \n
 Merienda tarde: 1 tostada integral con pur� de papa, cebolla y especias. \n
 Cena: Fideos salteados con tofu y soja. \n').
 
-dieta('Dieta 4',"proteica", "NA",'principiante',5,'
+               %----------------- Dieta 4 -----------------%
+
+dieta('Dieta 4',"proteica", "celiquia",'principiante','
 Puedes empezar con el siguiente plan alimenticio alto en prote�nas.\n
 Desayuno:   100g de at�n. \n
             3 huevos. \n
@@ -224,8 +148,10 @@ Almuerzo: 300g de bistec de res. \n
           Media taza de cebolla picada. \n
 Merienda tarde: 1 taza de fruta. \n
 Cena: 2 quesadillas de pollo. \n').
+
+               %----------------- Dieta 5 -----------------%
       
-dieta('Dieta 5',"NA", "NA",'principiante',5,'
+dieta('Dieta 5',"NA", "obesidad",'principiante','
 Puedes empezar con el siguiente plan alimenticio bajo en grasas.\n
 Desayuno:   1 vaso de leche descremada. \n
             20g de pan. \n
@@ -242,10 +168,103 @@ Cena: Sopa de verduras (75 g de repollo, 25 g de zanahoria, 50 g de nabo, 50 g d
       100 g de pollo asado. \n
       30 g de arroz. \n
       20 g de pan. \n').
+      
+               %----------------- Dieta 6 -----------------%
+      
+dieta('Dieta 6',"keto", "NA",'intermedio','
+Puedes empezar con el siguiente plan alimenticio bajo en carbohidratos.\n
+Desayuno:   � aguacate picado en rodaja.\n
+            2 huevos + rebanada de tocino.\n
+            Preparar solo rociando aceite en spray (no oliva ni vegetal). \n
+Merienda Ma�ana:  � pullado de semillas de girasol. \n
+        � naranja picada. \n
+Almuerzo: 2 tazas de ensalada Mixta.\n
+          2-3 tortitas de coliflor.\n
+          1 rebanada de pan keto.\n
+Merienda tarde: 1 taza de caf� o t�. \n
+                30g de mantequilla de man�. \n
+                1 muffin keto de ar�ndanos. \n
+Cena: � Tortilla de bacalao. \n
+      1 lata de at�n mediana en agua. \n
+      1 aguacate en trozos. \n
+').
 
-%----------------- AQUI VAN LAS DE MAU -----------------%
+               %----------------- Dieta 7 -----------------%
 
-dieta('Dieta 11',"keto", "NA",'avanzado',5,'
+
+dieta('Dieta 7',"vegetariana", "diabetes",'intermedio','
+Puedes empezar con el siguiente plan alimenticio vegetariano. \n
+Desayuno:   1 taza de leche de soya. \n
+            Pullado de pipas de girasol. \n
+            Banano en rebanadas. \n
+Merienda Ma�ana:  � taza de leche de coco. \n
+                  1 tostada de pan integral con mermelada. \n
+Almuerzo: 1 taza de Espaguetis con frutos secos. \n
+          � taza de sopa de quinua. \n
+          1 higo. \n
+Merienda tarde: 1 taza de jugo de naranja. \n
+                1 bocadillo de tofu y tomate. \n
+Cena: Ensalada de alga wakame con s�samo. \n
+      � rebanada de pan integral con pure y aguacate.\n
+').
+
+               %----------------- Dieta 8 -----------------%
+
+dieta('Dieta 8',"vegana", "dislipidemia",'intermedio','
+Puedes empezar con el siguiente plan alimenticio vegano. \n
+Desayuno: 1 smoothie bowl de pi�a y coco con frutos rojos. \n
+          1 galleta de avena y manzana. \n
+          � pieza de frutas fresca. \n
+Merienda Ma�ana:  1 taz�n de leche de soya . \n
+                  4-5 Almendras sin az�car y nueces. \n
+Almuerzo: 2 Alb�ndigas veganas de br�coli con garbanzos y arroz. \n
+          � taza de ensalada de vegetales. \n
+          1 trozo de mandarina. \n
+Merienda tarde: 1 vaso de bebida vegetal sin az�car. \n
+                1 barrita de avena y frutos secos. \n
+Cena:  Salteado r�pido de tofu y kale con s�samo. \n
+       � pimiento relleno de soja texturizada. \n
+').
+
+               %----------------- Dieta 9 -----------------%
+
+dieta('Dieta 9',"proteica", "celiaquia",'intermedio','
+Puedes empezar con el siguiente plan alimenticio alto en prote�nas. \n
+Desayuno: 2 huevos revueltos. \n
+          2 galletas de trigo con mantequilla de man�. \n
+          1 taza de caf�. \n
+Merienda Ma�ana: � Banano . \n
+                 1 galleta de prote�na Whey. \n
+Almuerzo: 1 taza de Ensalada mixta. \n
+          2 trozos de pechuga a la plancha. \n
+          1 higo. \n
+Merienda tarde: Batido de prote�na y creatina. \n
+                3-4 Almendras sin az�car. \n
+Cena: � taza de rodajas de tomate. \n
+      1 � de Filet de pescado al horno. \n
+      Papas salteadas con culantro. \n
+').
+
+               %----------------- Dieta 10 -----------------%
+
+dieta('Dieta 10',"NA", "obesidad",'intermedio','
+Puedes empezar con el siguiente plan alimenticio bajo en grasas. \n
+Desayuno: 2 huevos revueltos con aguacate y tomate. \n
+          Manzana en rebanadas. \n
+Merienda Ma�ana:  � taza de t� verde. \n
+                  1 bolsita de palitos de zanahoria. \n
+Almuerzo: Estofado de ternera con verduras. \n
+          � taza pejibayes. \n
+Merienda tarde: 1 taza de caf� o t�. \n
+                1 tostada con miel. \n
+Cena:  Verduras al horno. \n
+       Filet de res asado. \n
+       1 tortilla de ma�z a la brasa. \n
+').
+
+               %----------------- Dieta 11 -----------------%
+
+dieta('Dieta 11',"keto", "NA",'avanzado','
 Puedes empezar con el siguiente plan alimenticio bajo en carbohidratos.\n
 Desayuno:   Huevos revueltos con espinacas y salchicha fresca. \n
             2 huevos grandes + 1 cucharada de aceite de coco. \n
@@ -269,7 +288,9 @@ Cena: Solomillo con hongos y vainicas salteadas. \n
       50 gramos vaincas frescas + 2 cucharadas aceite de coco. \n
       Postre: 1 gelatina sin az�car gelatina (120 g). \n').
 
-dieta('Dieta 12',"Vegetariana", "Celiaquia",'avanzado',5,'
+               %----------------- Dieta 12 -----------------%
+
+dieta('Dieta 12',"vegetariana", "diabetes",'avanzado','
 Puedes empezar con el siguiente plan alimenticio vegetariano. \n
 Desayuno:   2 tostadas integrales (40g) con una cucharada de aceite de oliva. \n
             1 vaso de batido de soya enriquecida, con una cucharadita de cacao en polvo. \n
@@ -298,7 +319,9 @@ Cena: Crema de Calabaza. \n
       4 rebanadas de 1/4 de pan integral (70g). \n
 Merienda despu�s de la cena: 1 taza de fruta. \n').
 
-dieta('Dieta 13',"Vegana", "Dislipidemia",'avanzado',5,'
+               %----------------- Dieta 13 -----------------%
+
+dieta('Dieta 13',"vegana", "dislipidemia",'avanzado','
 Puedes empezar con el siguiente plan alimenticio vegano.\n
 Desayuno:   75g Copos de Avena. \n
             300ml de bebida de soja. \n
@@ -313,7 +336,9 @@ Cena: 100g de arroz basmati con br�coli, cebolla, pimiento, curry. \n
       200g de tofu sazonado con especias al gusto. \n
 Merienda despu�s de la cena: 1 banano. \n').
 
-dieta('Dieta 14',"Proteica", "Diabetes",'avanzado',5,'
+               %----------------- Dieta 14 -----------------%
+
+dieta('Dieta 14',"proteica", "celiaquia",'avanzado','
 Puedes empezar con el siguiente plan alimenticio alto en prote�nas.\n
 Desayuno:   Batido de naranja natural. \n
             1 tostada de mermelada. \n
@@ -330,7 +355,9 @@ Cena: 100g Pasta con pechuga de pollo. \n
       Queso fresco. \n
 Merienda despu�s de la cena: 1 taza de fruta. \n').
 
-dieta('Dieta 15',"NA", "NA",'avanzado',5,'
+               %----------------- Dieta 15 -----------------%
+
+dieta('Dieta 15',"NA", "obesidad",'avanzado','
 Puedes empezar con el siguiente plan alimenticio bajo en grasas.\n
 Desayuno:   180g de queso fresco 0%. \n
             100g de avena. \n
@@ -350,8 +377,11 @@ Cena: brocoli cocido con 2 huevos cocidos. \n
       200g ensalada de repollo y tomate. \n
 Merienda despu�s de la cena: 1 manzana. \n').
 
-%------------------ Programa ------------------%
 
+
+
+%------------------ Programa ------------------%
+/**
 % Mensajes predeterminados
 saludo:- write("NutriTEC: �Hola, encantado de ver que deseas mejorar tu estilo de vida! �En qu� te puedo ayudar? \n").
 enfermedad:- write("�Tienes alguna enfermedad por la que has iniciado este proceso?\n").
@@ -359,6 +389,6 @@ calorias:- write("�Tienes pensado una cantidad espec�fica de calor�as diar
 actividad:- write("�Cu�ntas veces a la semana realizas actividad f�sica?\n").
 tipoDieta:- write("�Tienes un tipo de dieta te gustar�a realizar?\n").
 despedida:- write("�Con mucho gusto!\n").
-
+  */
 
 
